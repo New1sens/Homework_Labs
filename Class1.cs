a118a23ba9637ec3b4fd5fac5cef4e60dd6a9f22
New1sens <ipin268@mail.ru>
2024-11-25 18:25:38 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    public class HighInterfaceArrayImpl : pHighInterfaceArray
+    {
+
+        private readonly long[] array;
+        private int nelems;
+
+        public HighInterfaceArrayImpl(int size)
+        {
+            array = new long[size];
+            nelems = 0;
+
+        }
+
+        public bool Find(long searchValue)
+        {
+            for (int i = 0; i < nelems; i++)
+            {
+                if (array[i] == searchValue)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Insert(long value)
+        {
+            array[nelems] = value;
+            nelems++;
+        }
+
+        public bool Delete(long value)
+        {
+            int i;
+            for (i = 0; i < nelems; i++)
+            {
+                if (array[i] == value)
+                {
+                    break;
+                }
+            }
+
+            if (i == nelems - 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int j = i; j < nelems - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+                nelems--;
+                return true;
+            }
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < nelems; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int GetSize()
+        {
+            return nelems;
+        }
+        public void Find()
+        {
+            if (nelems == 0)
+            {
+                Console.WriteLine("Массив пуст.");
+                return;
+            }
+            
+            long min = array[0];
+            long max = array[0];
+              
+             for (int i = 1; i < nelems; i++)
+            {
+                if (array[i] < max)
+                {
+                    max = array[i];
+                }
+                if (array[i] > min)
+                {
+                    min = array[i];
+                    
+                }
+
+            }
+            Console.WriteLine($"Минимальное значение: {min}");
+            Console.WriteLine($"Максимальное значение: {max}");
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    public class HighInterfaceClient
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int size = 100;
+            pHighInterfaceArray array = new HighInterfaceArrayImpl(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                array.Insert(NextLong64(random, 50));
+            }
+
+            array.Display();
+
+            ((HighInterfaceArrayImpl)array).Find();
+
+
+            long searchValue = NextLong64(random, 50);
+            if (array.Find(searchValue))
+            {
+                Console.WriteLine($"В массиве есть искомое значение, найдено. {searchValue}");
+            }
+            else
+            {
+                Console.WriteLine($"В массиве нет таких искомых значений. {searchValue}");
+            }
+            Console.WriteLine("Нажми пробел чтоб выйти");
+            Console.ReadKey();
+        }
+
+        public static long NextLong64(Random random, long maxValue)
+        {
+            byte[] buffer = new byte[8];
+            random.NextBytes(buffer);
+            long result = BitConverter.ToInt64(buffer, 0);
+
+            return Math.Abs(result % maxValue);
+        }
+    }
+}
