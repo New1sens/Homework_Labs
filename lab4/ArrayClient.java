aed8de41d4da2f684d65310912555440a7dae094
New1sens <ipin268@mail.ru>
2024-11-28 18:14:21 +0500

    Add files via upload

---
+++ lab4/ArrayClient.java
@@ -0,0 +1,60 @@
+package laba4;
+
+import java.util.Random;
+
+public class ArrayClient {
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+        int size = 20;
+
+        CommonArray commonArray = new CommonArray(size);
+        OrderedArray orderedArray = new OrderedArray(size);
+
+        for (int i = 0; i < size; i++) {
+            boolean insertResultCommonArray = false;
+            boolean insertResultOrderedArray = false;
+
+            while (!insertResultCommonArray) {
+                insertResultCommonArray = commonArray.insert(random.nextLong(1000));
+            }
+
+            while (!insertResultOrderedArray) {
+                insertResultOrderedArray = orderedArray.insert(random.nextLong(1000));
+            }
+        }
+
+        long searchValue = random.nextLong(1000);
+
+        orderedArray.display();
+
+        System.out.println("Минимальный элемент упоряд. массива: " + orderedArray.getMin());
+        System.out.println("Максимальный элемент упоряд.массива: " + orderedArray.getMax());
+
+        if (orderedArray.contains(searchValue)) {
+            System.out.println("Значение найдено в упоряд. массиве: " + searchValue);
+        } else {
+            System.out.println("Значение не найдено в упоряд.массиве: " + searchValue);
+        }
+        System.out.println("Количество операций в упоряд. массиве: " + orderedArray.getOperationsCount());
+
+
+
+        commonArray.display();
+
+        System.out.println("Минимальный элемент неупоряд. массива: " + commonArray.getMin());
+        System.out.println("Максимальный элемент неупоряд. массива: " + commonArray.getMax());
+
+        if (commonArray.contains(searchValue)) {
+            System.out.println("Значение найдено в неупоряд. массиве: " + searchValue);
+        } else {
+            System.out.println("Значение не найдено в неупоряд. массиве: " + searchValue);
+        }
+        System.out.println("Количество операций в неупоряд. массиве: " + commonArray.getOperationsCount());
+
+
+
+
+    }
+}
---
+++ lab4/CommonArray.java
@@ -0,0 +1,55 @@
+package laba4;
+
+
+public class CommonArray extends ParentLongArray {
+    public CommonArray(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean contains(long searchValue) {
+        operationsCount = 0; // Обнуляем счетчик перед поиском
+
+        for (int i = 0; i < nElems; i++) {
+            operationsCount++; // Увеличиваем счетчик операций
+            if (array[i] == searchValue) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public long getMin() {
+        if (nElems == 0) {
+            throw new IllegalStateException("Массив пуст.");
+        }
+
+        // Инициализация минимального значения первым элементом
+        long min = array[0];
+
+        // Поиск минимального значения в массиве
+        for (int i = 1; i < nElems; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public long getMax() {
+        if (nElems == 0) {
+            throw new IllegalStateException("Массив не имеет элементов.");
+        }
+
+        // Инициализация максимального значения первым элементом
+        long max = array[0];
+
+        // Поиск максимального значения в массиве
+        for (int i = 1; i < nElems; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
