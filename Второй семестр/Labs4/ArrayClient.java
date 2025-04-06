e746d2c85462bbe828f1603aa6eae19b061cc775
New1sens <ipin268@mail.ru>
2025-04-06 19:44:15 +0500

    dfy

---
+++ Второй семестр/Labs4/ArrayClient.java
@@ -0,0 +1,31 @@
+package Labs4;
+
+import java.util.Random;
+
+public class ArrayClient {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int size = 20;
+
+        CommonArray commonArray = new CommonArray(size);
+
+        // Заполнение массива случайными значениями
+        for (int i = 0; i < size; i++) {
+            commonArray.insert(random.nextLong(1000));
+        }
+
+        System.out.println("Массив до сортировки:");
+        commonArray.display();
+
+        // Быстрая сортировка
+        long startTime = System.nanoTime();
+        commonArray.quickSort();
+        long endTime = System.nanoTime();
+
+        System.out.println("Массив после сортировки:");
+        commonArray.display();
+
+        System.out.println("Время выполнения быстрой сортировки: " + (endTime - startTime) + " нс");
+        System.out.println("Количество операций: " + commonArray.getOperationsCount());
+    }
+}
